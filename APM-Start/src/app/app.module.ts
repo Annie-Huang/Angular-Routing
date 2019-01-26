efce6339a52e203c686d29b9075600eb9af995c0
Annie Huang <annie.hn@gmail.com>
2019-01-26 13:44:18 +1100

    Reverse all the changes as I checked in the wrong app.module.ts (should not do that one under APM-Start directory.

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -16,22 +15,11 @@ import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
 
-// A key point to keep in mind is that the order of the routes in this array matters.
-//   The router uses a first match wins strategy when matching route paths.
-//   This means that more specific paths should always be before less specific paths, such as the wildcard path.
-// Redirects can be local or absolute. Local redirects replace a single URL segment with a different one, such as our examples here.
-//   An absolute redirect replaces the entire URL.
-
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
-    RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', component: PageNotFoundComponent }
-    ]),
     ProductModule,
     UserModule,
     MessageModule
