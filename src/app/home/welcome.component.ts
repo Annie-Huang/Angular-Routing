efc2d17384ba9ef78c15f264419f6ac675bd9b99
Annie Huang <annie.hn@gmail.com>
2019-01-26 13:45:50 +1100

    Put template in for router-outlet

--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+// A component only needs a selector if it will be used as a nested component.
+// Since we are now routing to the Welcome component instead of using it as a nested component,
+// we no longer need the selector.
+
+//   selector: 'pm-home',
 @Component({
-  selector: 'pm-home',
   templateUrl: './welcome.component.html'
 })
 export class WelcomeComponent {
