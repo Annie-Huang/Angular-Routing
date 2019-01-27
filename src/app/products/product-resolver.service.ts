eb7414a7edd7221cc5d3d7354d44bf3062e2a09c
Annie Huang <annie.hn@gmail.com>
2019-01-27 20:54:11 +1100

    Add logic to check params

--- src/app/products/product-resolver.service.ts
+++ src/app/products/product-resolver.service.ts
@@ -16,10 +16,15 @@ export class ProductResolver implements Resolve<Product> {
   // Love auto complete from Intellij!
   // resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> | Promise<Product> | Product {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
-    const id = +route.paramMap.get('id');
+    const id = route.paramMap.get('id');
+    if (isNaN(+id)) {
+      const message = `Product id was not a number: ${id}`;
+      console.error(message);
+      return;
+    }
 
     // Notice that we don't subscribe here, the resolver manages the subscription for us and does not continue
     // until the data is returned and the subscription is complete.
-    return this.productService.getProduct(id);
+    return this.productService.getProduct(+id);
   }
 }
