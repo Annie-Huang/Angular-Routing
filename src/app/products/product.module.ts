e76b58422999a160904df69b21a614d440c47ed8
Annie Huang <annie.hn@gmail.com>
2019-01-27 21:16:34 +1100

    Add resolver to route configuration

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -4,16 +4,28 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductResolver } from "./product-resolver.service";
 
 import { SharedModule } from '../shared/shared.module';
 
+// To associate a route resolver with a route, we add it to the route configuration that we define within an Angular module.
+//   This ensures that the data for the route is retrieved using the resolver before the routed component is activated.
+// When the product detail route is activated, this route configuration tells the router to use the resolver service to prefetch the product data.
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
-      {path: 'products/:id', component: ProductDetailComponent},
-      {path: 'products/:id/edit', component: ProductEditComponent}
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent,
+        resolve: {resolvedData: ProductResolver}
+      },
+      {
+        path: 'products/:id/edit',
+        component: ProductEditComponent,
+        resolve: {resolvedData: ProductResolver}
+      }
     ])
   ],
   declarations: [
