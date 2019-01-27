c0ac3901cb0221757d66c3e0c11d8af771978b5c
Annie Huang <annie.hn@gmail.com>
2019-01-27 21:32:10 +1100

    Get product-detail.component to read resolve data so the whole page will be loaded rather than partial loading at the beginning.

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Product } from './product';
-import { ProductService } from './product.service';
+import {Product, ProductResolved} from './product';
+// import { ProductService } from './product.service';
 import {AuthService} from "../user/auth.service";
 
 @Component({
@@ -14,20 +14,29 @@ export class ProductDetailComponent implements OnInit{
   product: Product;
   errorMessage: string;
 
+/*
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
+*/
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // Since the URL is a string, we add a plus sign at the front to cast it to a number.
+/*    // Since the URL is a string, we add a plus sign at the front to cast it to a number.
     const id = +this.route.snapshot.paramMap.get('id');
-    this.getProduct(id);
+    this.getProduct(id);*/
+
+    const resolvedData: ProductResolved = this.route.snapshot.data['resolvedData'];
+    this.errorMessage = resolvedData.error;
+    this.onProductRetrieved(resolvedData.product);
+    // Try http://localhost:4200/products/5?filterBy=&showImage=false for success scenario.
+    // http://localhost:4200/products/25?filterBy=&showImage=false for fail scenario: there is no product 25
   }
 
-  getProduct(id: number) {
-    this.productService.getProduct(id).subscribe(
-      product => this.onProductRetrieved(product),
-      error => this.errorMessage = <any>error);
-  }
+  // getProduct(id: number) {
+  //   this.productService.getProduct(id).subscribe(
+  //     product => this.onProductRetrieved(product),
+  //     error => this.errorMessage = <any>error);
+  // }
 
   onProductRetrieved(product: Product): void {
     this.product = product;
