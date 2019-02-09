e02152aeee8b9a56a511e4e86bff0894afbef432
Annie Huang <annie.hn@gmail.com>
2019-02-09 13:59:35 +1100

    { preloadingStrategy: SelectiveStrategy }

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,26 @@ import {PreloadAllModules, RouterModule} from "@angular/router";
 import {WelcomeComponent} from "./home/welcome.component";
 import {PageNotFoundComponent} from "./page-not-found.component";
 import {AuthGuard} from "./user/auth.guard";
+import {SelectiveStrategy} from "./selective-strategy.service";
 
 // RouterModule.forRoot([..], { enableTracing: true }),
 // And watch route event in chrome developer tool.
+// { preloadingStrategy: PreloadAllModules }
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
       {
         path: 'products',
-        canActivate: [AuthGuard],
+        // canActivate: [AuthGuard],
         // canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
+        data: { preload: true },
         loadChildren: './products/product.module#ProductModule'
       },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', component: PageNotFoundComponent }
-    ], { preloadingStrategy: PreloadAllModules }),  // canLoad guard blocks preloading
+    ], { preloadingStrategy: SelectiveStrategy }),  // canLoad guard blocks preloading
   ],
   exports: [RouterModule]
 })
